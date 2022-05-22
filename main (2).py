8ddf09512075dd4865d60f5ff8fe2934bf645cdf
LUCKY TIWARI <89370329+Loki1920@users.noreply.github.com>
2022-05-22 22:46:41 +0530

    Add files via upload

---
+++ main (2).py
@@ -0,0 +1,17 @@
+#selection sort 
+list1 = [56,3,2,78,6,0]
+# sorting in ascending order 
+for i in range(len(list1)):
+  min_value = min(list1[i:])
+  min_index = list1.index(min_value)
+  list1[i],list1[min_index] = list1[min_index],list1[i]
+
+print(list1)
+
+# sorting in descending order 
+for j in range(len(list1)):
+  min_value = max(list1[j:])
+  min_index = list1.index(min_value)
+  list1[j],list1[min_index] = list1[min_index],list1[j]
+
+print(list1)
